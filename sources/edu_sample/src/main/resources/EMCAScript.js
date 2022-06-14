e610a9b05614e2a217e22001e3f701d3f9fd91e0
SEUNGHWA JEONG <keepinmindsh@gmail.com>
2022-06-15 06:17:13 +0900

    script sample

--- sources/edu_sample/src/main/resources/EMCAScript.js
+++ sources/edu_sample/src/main/resources/EMCAScript.js
@@ -1,4 +1,37 @@
 
 const callFunc = () => {
-
+    console.log("Hello!")
 }
+
+callFunc();
+
+const restFunc = (x, ...y) => {
+    // y is an Array
+    return x * y.length;
+}
+
+console.log(restFunc(3, "hello", true) == 6);
+
+const spreadFunc = (x, y, z) => {
+    return x + y + z;
+}
+// Pass each elem of array as argument
+console.log(spreadFunc(...[1,2,3]) == 6);
+
+if(true){
+    let value = 100;
+}
+
+const constValue = 100;
+
+function func(){
+
+    if(true){
+        let value = 100;
+    }
+
+    console.log(value);
+}
+
+
+func();
